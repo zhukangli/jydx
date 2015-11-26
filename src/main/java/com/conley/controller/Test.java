cb58bb11d3b1f20f2e9d5ef558f8c3c67152e87d
Conley <276012105@qq.com>
2015-11-26 10:52:52 +0800

    spring-mvc集成提交

--- src/main/java/com/conley/controller/Test.java
+++ src/main/java/com/conley/controller/Test.java
@@ -1,9 +1,17 @@
 package com.conley.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("1111zhukangli");
-	}
+	@RequestMapping("/hello")
+	public ModelAndView hello(){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("meaasge","zhukanmgli");
+		mv.setViewName("hello");
+		return mv;
+	}	
 
 }
